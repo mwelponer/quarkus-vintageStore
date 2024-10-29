cad73ab946b422729f160759483bdc04c84468e0
mike <michele.welponer@tndigit.it>
2024-10-29 16:24:40 +0100

    initialize repo, bootstrap of two microservices

---
+++ quarkus-rest-number/src/test/java/org/acme/NumberResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class NumberResourceIT extends NumberResourceTest {
+    // Execute the same tests but in packaged mode.
+}
