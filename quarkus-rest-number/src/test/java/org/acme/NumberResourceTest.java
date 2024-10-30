75d7f5c32be8918bf8a7c31f96d690de71ad0bc2
mike <michele.welponer@tndigit.it>
2024-10-30 14:11:28 +0100

    test number microservice

--- quarkus-rest-number/src/test/java/org/acme/NumberResourceTest.java
+++ quarkus-rest-number/src/test/java/org/acme/NumberResourceTest.java
@@ -4,17 +4,21 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.hasKey;
 
 @QuarkusTest
 class NumberResourceTest {
+
     @Test
-    void testHelloEndpoint() {
+    void test_NumberResource() {
         given()
-          .when().get("/hello")
+          .when().get("/api/numbers")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body("ISBN_13", startsWith("13-"))
+             .body("ISBN_10", startsWith("10-"))
+             .body(not(hasKey("generationDate")));
     }
-
 }
